0600690d28a4d72731a881eea6f658dc222afd7c
yra1908 <yra1908@gmail.com>
2015-10-29 22:45:18 +0200

    second

--- app/src/main/java/com/testtask/currency/MainActivity.java
+++ app/src/main/java/com/testtask/currency/MainActivity.java
@@ -1,31 +1,49 @@
 package com.testtask.currency;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.testtask.currency.service.HttpManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView output;
+    List<MyTask> tasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        output = (TextView) findViewById(R.id.textView);
+        output.setMovementMethod(new ScrollingMovementMethod());
+
+        tasks = new ArrayList<>();
+    }
+
+    public void btnClickHandlerSecondActivity(View view) {
+        Intent secondActivity = new Intent(this, CurrencyLogActivity.class);
+        startActivity(secondActivity);
+
+    }
+
+    public void btnClickHandlerThirdActivity(View view) {
+        Intent thirdActivity = new Intent(this, CurrencyLogActivity.class);
+        startActivity(thirdActivity);
     }
 
     @Override
@@ -35,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    //not using yet
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -43,10 +62,80 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_currency_graph) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void btnClickHandlerThirdActivity2(MenuItem item) {
+        Intent thirdActivity = new Intent(this,  CurrencyGraphActivity.class);
+        startActivity(thirdActivity);
+    }
+
+    public void btnClickHandlerSecondActivity2(MenuItem item) {
+        Intent secondActivity = new Intent(this,  CurrencyLogActivity.class);
+        startActivity(secondActivity);
+    }
+
+    public void btnClickHandlerFirstActivity2(MenuItem item) {
+        Intent firstActivity = new Intent(this,  MainActivity.class);
+        startActivity(firstActivity);
+    }
+
+
+    private void requestData(String uri) {
+        MyTask task = new MyTask();
+        task.execute(uri);
+    }
+
+    public void getCurrencyRate(View view) {
+
+        if (isOnline()) {
+            requestData("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5");
+        } else {
+            Toast.makeText(this, "Network isn't available", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void updateDisplay(String message) {
+        output.append(message + "\n");
+    }
+
+    protected boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private class MyTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String content = HttpManager.getData(params[0]);
+            return content;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            updateDisplay(result);
+
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            updateDisplay(values[0]);
+        }
+
     }
 }
